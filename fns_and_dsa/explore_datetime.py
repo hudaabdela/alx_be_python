2f893cb13e035f10b489803da7ed2bac53b199d5
Huda Abdela <hudaabdulah75@gmail.com>
2024-09-29 19:41:43 +0300

    third python

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+import datetime
+def display_current_datetime() :
+    current_date = datetime.datetime.now()
+    print(f"Current date and time: {current_date}")
+display_current_datetime()
+
+from datetime import timedelta
+def calculate_future_date() :
+    number_of_days = int(input("Enter the number of days to add to the current date: "))
+    current_date = datetime.datetime.now()
+    future_date = current_date + timedelta(days = number_of_days )
+    print(f"Future date: {future_date}")
+calculate_future_date()
---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+shopping_list = []
+def display_menu () :
+    print("Shopping List Manager")
+    print("1.Add Item")
+    print("2.Remove Item")
+    print("3.View List")
+    print("4.Exit")
+def main() :
+    shopping_list = []
+    while True :
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1" : 
+            a = input("Add item: ")
+            shopping_list.append(a)
+        elif choice == "2" :
+            y = input("Remove item : ")
+            shopping_list.remove(y)
+        elif choice == "3" :
+            for v in shopping_list:
+                print(f"-{v}")
+        elif choice == "4" :
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+if __name__ == "__main__":
+    main()
+
